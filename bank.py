669f530e5c371f6d84ec2f46c97594db92d694d2
Jon Leete <jleete97@gmail.com>
2017-09-06 20:11:53 -0400

    first working model

--- bank.py
+++ bank.py
@@ -1,1 +1,23 @@
 # this is a bank
+
+class Bank:
+
+    def __init__(self):
+        self.accounts = { }
+
+    def withdraw(self, account, amount):
+        self.accounts[account] = self.accounts[account] - amount
+
+    def deposit(self, account, amount):
+        self.accounts[account] = self.accounts[account] + amount
+
+    def check_balance(self, account):
+        return self.accounts[account]
+
+    def create_account(self, account):
+        self.accounts[account] = 0
+
+    def transfer(self, account_from, account_to, amount):
+        self.accounts[account_to] = self.accounts[account_to] + amount
+        self.accounts[account_from] = self.accounts[account_from] - amount
+
